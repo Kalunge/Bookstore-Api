e2ce7d0154542c5a439be4e4cd5c84031fbc319c
kalunge <muthomititus+github@gmail.com>
2022-02-08 18:20:05 +0100

    Add create action for books

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -4,19 +4,18 @@ class Api::V1::BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(book_params)
-    if @book.save
-      render json: @book, :created
+    book = Book.new(book_params)
+    if book.save
+      render json: book, status: :created
     else
-      render :unprocessable_entity
+      render json: book.errors,  status: :unprocessable_entity
     end
-
   end
 
 
   private
 
   def book_params
-    params.require(:book).permit(:name, :author)
+    params.require(:book).permit(:title, :author)
   end
 end
