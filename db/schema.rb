02c50db8404ea839b12da1c9f11f9751afdcb424
kalunge <muthomititus+github@gmail.com>
2022-02-09 16:35:22 +0100

    Add Books Author associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_08_165925) do
+ActiveRecord::Schema.define(version: 2022_02_09_152700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "author"
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-books = Book.create!([{title: "The meaning of Marriage", author: "Timothy Keller"}, {title: "The Freedom of Self forgetfulbess", author: "Timothy Keller"}, {title: "The Fear of Man", author: "Welch"}, {title: "12 Rules for Life", author: "Jordan B Peterson"}])
+authors = Author.create!([{first_name: "Timothy ", last_name:"Keller", age:70}, {first_name: "Robert Chire ", last_name:"Sproul", age:78}, {first_name: "John ", last_name:"Piper", age:68}])
+
+books = Book.create!([{title: "The meaning of Marriage", author: Author.first}, {title: "The Freedom of Self forgetfulbess", author: Author.second}, {title: "The Fear of Man", author: Author.third}, {title: "12 Rules for Life", author: Author.first}])
