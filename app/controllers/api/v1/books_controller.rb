a9f947ba75bbaecf9af36a9902eed07957ceedfb
kalunge <muthomititus+github@gmail.com>
2022-02-09 19:48:47 +0100

    Add pagination logic

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,12 +1,18 @@
+# require 'net/http'
+
 class Api::V1::BooksController < ApplicationController
+  MAX_PAGINATION_LIMIT = 100
   def index
-    books = Book.limit(params[:limit]).offset(params[:offset])
+    books = Book.limit(limit).offset(params[:offset])
     render json: BooksRepresenter.new(books).as_json
   end
 
   def create
     author = Author.create!(author_params)
     book = author.books.create!(book_params)
+
+    # uri = URI('http://localhost:4567//update_sku')
+    # Net::HTTP::Post.new(uri, 'Content-TYpe' => "application/json")
     if book.save
       render json: BookRepresenter.new(book).as_json, status: :created
     else
@@ -21,6 +27,10 @@ class Api::V1::BooksController < ApplicationController
   end
 
   private
+
+  def limit
+    [params.fetch(:limit, MAX_PAGINATION_LIMIT).to_i, 100].min
+  end
 
   def author_params
     params.require(:author).permit(:first_name, :last_name, :age)
