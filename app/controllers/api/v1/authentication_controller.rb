b985d6fddebdf24088c4cadd8a403b3e331745bd
kalunge <muthomititus+github@gmail.com>
2022-02-10 12:31:50 +0100

    Update authentication controller to handle exception

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::AuthenticationController < ApplicationController
+
+  rescue_from ActionController::ParameterMissing, with: :parameter_missing
   def create
-    p params.inspect
+    p params.require(:username).inspect
+    p params.require(:password).inspect
     render json: { token: '123' }, status: :created
+  end
+
+  private
+  def parameter_missing
+    render json: {errors: "param is missing or the value is "}, status: :unprocessable_entity
   end
 end
--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -14,10 +14,16 @@ describe 'Authentication', type: :request do
     it 'Return error when username is missing' do
       post '/api/v1/authenticate', params: { username: 'kalunge' }
       expect(response).to have_http_status(:unprocessable_entity)
-    end
-    it 'Return error when password is missing' do
-      post '/api/v1/authenticate', params: { password: 'kalunge' }
-      expect(response).to have_http_status(:unprocessable_entity)
+      expect(response_body).to eq({
+        "errors" => "param is missing or the value is "
+        })
+      end
+      it 'Return error when password is missing' do
+        post '/api/v1/authenticate', params: { password: 'kalunge' }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response_body).to eq({
+         "errors" => "param is missing or the value is "
+      })
     end
   end
 end
