8af696bdf7052961b1fb2dd17ec27879d2eadc02
kalunge <muthomititus+github@gmail.com>
2022-02-10 14:05:03 +0100

    Add tests for unmatching password

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 describe 'Authentication', type: :request do
-  let(:user) {FactoryBot.create(:user, username: "Kalunge")}
+  let(:user) {FactoryBot.create(:user, username: "Kalunge", password: "123")}
   describe 'POST /authenticate' do
     it 'Authenticates the client and returns a token' do
       post '/api/v1/authenticate', params: { username: user.username, password: 'qwerty' }
 
       expect(response).to have_http_status(:created)
       expect(response_body).to eq({
-                                    'token' => '123'
+                                    'token' => 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.DiPWrOKsx3sPeVClrm_j07XNdSYHgBa3Qctosdxax3w'
                                   })
     end
 
@@ -25,6 +25,11 @@ describe 'Authentication', type: :request do
       expect(response_body).to eq({
                                     'errors' => 'Username or Password is missing'
                                   })
+    end
+    it "Returns error when password is incorrect" do
+      post '/api/v1/authenticate', params: { username: user.username,password: 'kalunge' }
+
+      expect(response).to have_http_status(:unauthorized)
     end
   end
 end
