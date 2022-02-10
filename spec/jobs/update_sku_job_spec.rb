a9d0cba95e0df773b7048e06277e38b99700fead
kalunge <muthomititus+github@gmail.com>
2022-02-10 09:52:11 +0100

    Update active job class to get correct Params

--- spec/jobs/update_sku_job_spec.rb
+++ spec/jobs/update_sku_job_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe UpdateSkuJob, type: :job do
+  let(:book_title) { 'Eloquent Ruby' }
   it "it calls sku service with the correct params" do
     allow(Net::HTTP).to receive(:start).and_return(true)
+
+    expect_any_instance_of(Net::HTTP::Post).to receive(:body=).with(
+      {sku: '123', title: book_title}.to_json
+    )
     described_class.perform_now('Eloquent Ruby')
   end
 end
